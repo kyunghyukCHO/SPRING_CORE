44e7e005efc079bd6d598c33b671f7c410d8bde5
kyunghyukcho <rudgur1996@hanmail.net>
2022-07-28 21:46:31 +0900

    second commit

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,22 +1,42 @@
 package hello.core;
 
-import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     // 생성자 주입
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
+    public MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
+    public DiscountPolicy discountPolicy() { // 인터페이스 참조.. 자식 구현체 참조는 how ??
+        //return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+    }
 
 }
+
+
+// 정적인 다이어그램을 손대지 않는다 : 애플리케이션 코드를 손대지 않는다 -> 의존관계를 바꾸찌 않음
+// 동적 다이어그을 쉽게 바꿀 수 있음
